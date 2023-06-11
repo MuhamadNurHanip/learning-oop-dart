d40cb514104f9f7b0a9e5dd07664e0aa92667834
Hanif <nurhanif2312@gmail.com>
2023-06-11 21:46:06 +0700

    update

--- class.dart
+++ class.dart
@@ -1,4 +1,4 @@
-import 'dart:io'; //import library 
+import 'dart:io'; // Mengimpor library dart:io untuk input/output
 
 // Abstract class untuk representasi Pengguna (User)
 abstract class User {
@@ -6,30 +6,30 @@ abstract class User {
   String _email;
   String _phoneNumber;
 
-  User(this._name, this._email, this._phoneNumber); //constructor
+  User(this._name, this._email, this._phoneNumber); // Constructor
 
-  String get name => _name; // konsep enkapsulasi
+  String get name => _name; // Getter untuk mengakses nilai properti _name dari luar kelas User
   set name(String value) {
-    _name = value;
+    _name = value; // Setter untuk mengubah nilai properti _name
   }
 
-  String get email => _email;
+  String get email => _email; // Getter untuk mengakses nilai properti _email dari luar kelas User
   set email(String value) {
-    _email = value;
+    _email = value; // Setter untuk mengubah nilai properti _email
   }
 
-  String get phoneNumber => _phoneNumber;
+  String get phoneNumber => _phoneNumber; // Getter untuk mengakses nilai properti _phoneNumber dari luar kelas User
   set phoneNumber(String value) {
-    _phoneNumber = value;
+    _phoneNumber = value; // Setter untuk mengubah nilai properti _phoneNumber
   }
 
-  void showInfo(); //akan ditimpa ovveride oleh kelas turunannya 
+  void showInfo(); // Metode abstrak yang akan ditimpa (override) oleh kelas turunannya
 }
 
-// Class turunan dari User untuk representasi Pelanggan (Customer)
+// Kelas turunan dari User untuk representasi Pelanggan (Customer)
 class Customer extends User {
   Customer(String name, String email, String phoneNumber)
-      : super(name, email, phoneNumber);
+      : super(name, email, phoneNumber); // Memanggil constructor kelas User
 
   @override
   void showInfo() {
@@ -40,16 +40,16 @@ class Customer extends User {
   }
 }
 
-// Class turunan dari User untuk representasi Pegawai (Staff)
+// Kelas turunan dari User untuk representasi Pegawai (Staff)
 class Staff extends User {
   String _department;
 
   Staff(String name, String email, String phoneNumber, this._department)
-      : super(name, email, phoneNumber);
+      : super(name, email, phoneNumber); // Memanggil constructor kelas User
 
-  String get department => _department;
+  String get department => _department; // Getter untuk mengakses nilai properti _department dari luar kelas Staff
   set department(String value) {
-    _department = value;
+    _department = value; // Setter untuk mengubah nilai properti _department
   }
 
   @override
@@ -62,7 +62,7 @@ class Staff extends User {
   }
 }
 
-// Class untuk representasi Ruangan (Room)
+// Kelas untuk representasi Ruangan (Room)
 class Room {
   String _roomName;
   int _roomNumber;
@@ -71,15 +71,15 @@ class Room {
   bool _isOccupied;
 
   Room(this._roomName, this._roomNumber, this._floor, this._facilities,
-      this._isOccupied);
+      this._isOccupied); // Constructor
 
-  String get roomName => _roomName;
-  int get roomNumber => _roomNumber;
-  int get floor => _floor;
-  List<String> get facilities => _facilities;
-  bool get isOccupied => _isOccupied;
+  String get roomName => _roomName; // Getter untuk mengakses nilai properti _roomName dari luar kelas Room
+  int get roomNumber => _roomNumber; // Getter untuk mengakses nilai properti _roomNumber dari luar kelas Room
+  int get floor => _floor; // Getter untuk mengakses nilai properti _floor dari luar kelas Room
+  List<String> get facilities => _facilities; // Getter untuk mengakses nilai properti _facilities dari luar kelas Room
+  bool get isOccupied => _isOccupied; // Getter untuk mengakses nilai properti _isOccupied dari luar kelas Room
   set isOccupied(bool value) {
-    _isOccupied = value;
+    _isOccupied = value; // Setter untuk mengubah nilai properti _isOccupied
   }
 
   void showInfo() {
@@ -95,7 +95,7 @@ class Room {
   }
 }
 
-// Class untuk representasi Pembayaran (Payment)
+// Kelas untuk representasi Pembayaran (Payment)
 class Payment {
   double _amount;
   String _paymentMethod;
@@ -104,23 +104,23 @@ class Payment {
 
   Payment(this._amount, this._paymentMethod)
       : _isPaid = false,
-        _dueDate = DateTime.now().add(Duration(days: 3));
+        _dueDate = DateTime.now().add(Duration(days: 3)); // Constructor
 
-  double get amount => _amount;
+  double get amount => _amount; // Getter untuk mengakses nilai properti _amount dari luar kelas Payment
   set amount(double value) {
-    _amount = value;
+    _amount = value; // Setter untuk mengubah nilai properti _amount
   }
 
-  String get paymentMethod => _paymentMethod;
+  String get paymentMethod => _paymentMethod; // Getter untuk mengakses nilai properti _paymentMethod dari luar kelas Payment
   set paymentMethod(String value) {
-    _paymentMethod = value;
+    _paymentMethod = value; // Setter untuk mengubah nilai properti _paymentMethod
   }
 
-  bool get isPaid => _isPaid;
-  DateTime get dueDate => _dueDate;
+  bool get isPaid => _isPaid; // Getter untuk mengakses nilai properti _isPaid dari luar kelas Payment
+  DateTime get dueDate => _dueDate; // Getter untuk mengakses nilai properti _dueDate dari luar kelas Payment
 
   void payPayment() {
-    _isPaid = true;
+    _isPaid = true; // Metode untuk melakukan pembayaran
   }
 
   void showInfo() {
--- main.dart
+++ main.dart
@@ -1,51 +1,45 @@
 import 'dart:io';
-import 'class.dart';
+import 'class.dart'; // import class dart 
+
+
 
 void main() {
   // Membuat objek Pelanggan (Customer) dan menampilkan informasinya
   print('Masukkan Informasi Pelanggan:');
   print('Nama: ');
-  String name = stdin.readLineSync()!;
+  String name = stdin.readLineSync()!; // Mengambil input nama dari pengguna
   print('Email: ');
-  String email = stdin.readLineSync()!;
+  String email = stdin.readLineSync()!; // Mengambil input email dari pengguna
   print('Nomor Telepon: ');
-  String phoneNumber = stdin.readLineSync()!;
-  Customer customer = Customer(name, email, phoneNumber);
-  customer.showInfo();
+  String phoneNumber = stdin.readLineSync()!; // Mengambil input nomor telepon dari pengguna
+  Customer customer = Customer(name, email, phoneNumber); // Membuat objek customer berdasarkan input pengguna
+  customer.showInfo(); // Menampilkan informasi pelanggan
   print('');
 
   // Menampilkan informasi kamar Hotel
   print('Informasi Kamar Hotel:');
   Room room = Room('Standart', 001, 2,
-      ['AC', 'TV', 'WI-FI', 'Lemari', 'Kamar Mandi'], false);
-  room.showInfo();
+      ['AC', 'TV', 'WI-FI', 'Lemari', 'Kamar Mandi'], false); // Membuat objek room dengan nilai properti yang ditentukan
+  room.showInfo(); // Menampilkan informasi kamar
   print('');
 
   // Membuat objek Pembayaran (Payment) dan menampilkan informasinya
   print('Masukkan Informasi Pembayaran:');
   print('Jumlah: ');
-  double amount = double.parse(stdin.readLineSync()!);
+  double amount = double.parse(stdin.readLineSync()!); // Mengambil input jumlah pembayaran dari pengguna
   print('Metode Pembayaran: ');
-  String paymentMethod = stdin.readLineSync()!;
-  Payment payment = Payment(amount, paymentMethod);
-  payment.showInfo();
+  String paymentMethod = stdin.readLineSync()!; // Mengambil input metode pembayaran dari pengguna
+  Payment payment = Payment(amount, paymentMethod); // Membuat objek payment berdasarkan input pengguna
+  payment.showInfo(); // Menampilkan informasi pembayaran
 
   // Meminta pengguna untuk melakukan pembayaran
   print('\nSilakan lakukan pembayaran sebelum ${payment.dueDate}');
   print('Apakah Anda sudah melakukan pembayaran? (ya/tidak)');
-  String confirmation = stdin.readLineSync()!.toLowerCase();
+  String confirmation = stdin.readLineSync()!.toLowerCase(); // Mengambil input konfirmasi pembayaran dari pengguna
   if (confirmation == 'ya') {
-    payment.payPayment();
+    payment.payPayment(); // Melakukan pembayaran
     print('Pembayaran berhasil.');
   } else {
     print('Pembayaran belum dilakukan.');
-  }
-
-  payment.showInfo();
-
-  // Estimasi waktu kamar hotel
-  if (room.isOccupied) {
-    DateTime estimatedCheckOut = DateTime.now().add(Duration(days: 1));
-    print('Estimasi Check-out: $estimatedCheckOut');
   }
 }
